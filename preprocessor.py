06bedf656a7fc6f32d0ff14d7ecdfd78de435e0e
questionmark4444 <bergerpintoj@stu.otc.school.nz>
2025-07-12 10:20:44 +1200

    creating new format for questions and answer storage

--- preprocessor.py
+++ preprocessor.py
@@ -1,10 +1,25 @@
 import json
 
-# Open and read the JSON file
-data = json.load(open('questions.json', 'r'))
+# Open and read the JSON files
+data1 = json.load(open('QuestionsNewFormat.json', 'r'))
+data2 = json.load(open('questions.json', 'r'))
 
-for x in range(int(len(data)/4)):
+question = ""
+x = 0
+while x < len(data1):
+    if type(data1[x]) == str:
+        question = data1[x]
+    else:
+        for i in range(128):
+            print(f"")
+        print(f"question: \"{question}\"")
+        print(f"answer: \"{data1[x][2]}\"")
+    x += 1
+
+x = 0
+while x < int(len(data2)/4):
     for i in range(128):
         print(f"")
-    print(f"question: \"{data[x*4+1]}\"")
-    print(f"answer: \"{data[x*4+3]}\"")
+    print(f"question: \"{data2[x*4+1]}\"")
+    print(f"answer: \"{data2[x*4+3]}\"")
+    x += 1
