a92322adc10fc13de9f7372d4b52f1d22cfeedb7
questionmark4444 <bergerpintoj@stu.otc.school.nz>
2025-07-13 13:10:33 +1200

    fully reformated question data

--- preprocessor.py
+++ preprocessor.py
@@ -1,24 +1,15 @@
 import json
 
-# Open and read the JSON files
-data1 = json.load(open('QuestionsNewFormat.json', 'r'))
-data2 = json.load(open('questions.json', 'r'))
+# Open and read the JSON file
+data = json.load(open('questions.json', 'r'))
 
 question = ""
 x = 0
-while x < int(len(data1)/2):
-    question = data1[x*2]
-    for list_index in range(3):
+while x < int(len(data)/2):
+    question = data[x*2]
+    for answer in data[x*2+1]:
         for i in range(128):
             print(f"")
         print(f"question: \"{question}\"")
-        print(f"answer: \"{data1[x*2+1][list_index]}\"")
-    x += 1
-
-x = 0
-while x < int(len(data2)/4):
-    for i in range(128):
-        print(f"")
-    print(f"question: \"{data2[x*4+1]}\"")
-    print(f"answer: \"{data2[x*4+3]}\"")
+        print(f"answer: \"{answer}\"")
     x += 1
